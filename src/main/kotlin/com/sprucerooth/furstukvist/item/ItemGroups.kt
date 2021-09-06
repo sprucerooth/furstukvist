0cf4dea0cb4a69407ba47d1dd429e07e7e500ba4
Jens Granroth <sprucerooth@protonmail.com>
2021-09-06 18:11:30 +0200

    Changed item group icon

--- src/main/kotlin/com/sprucerooth/furstukvist/item/ItemGroups.kt
+++ src/main/kotlin/com/sprucerooth/furstukvist/item/ItemGroups.kt
@@ -4,11 +4,10 @@ import com.sprucerooth.furstukvist.FurstuKvist
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
-import net.minecraft.item.Items
 import net.minecraft.util.Identifier
 
 @Suppress("UNUSED")
 object ItemGroups {
     val GROUP_GENERAL: ItemGroup =
-        FabricItemGroupBuilder.build(Identifier(FurstuKvist.MOD_ID, "general")) { ItemStack(Items.COBBLESTONE) }
+        FabricItemGroupBuilder.build(Identifier(FurstuKvist.MOD_ID, "general")) { ItemStack(Items.PAINT_BRUSH_RED) }
 }
